32e97ca2e3afd2b50bb6684dde54d5d02b2198a0
Francis <remy.francis@epfedu.fr>
2024-05-24 17:41:33 +0200

    'newpush'

--- function_create_groups.py
+++ function_create_groups.py
@@ -2,10 +2,10 @@ import function_database
 import sqlite3
 import numpy as np
 
-def get_students_per_class(number_student, slot_nbr_for_lv):
-    base_students_per_class = number_student // slot_nbr_for_lv
+def get_students_per_SCHOOL_YEAR(number_student, slot_nbr_for_lv):
+    base_students_per_promo = number_student // slot_nbr_for_lv
     extra_students = number_student % slot_nbr_for_lv
-    students_distribution = [base_students_per_class] * slot_nbr_for_lv
+    students_distribution = [base_students_per_promo] * slot_nbr_for_lv
     for i in range(extra_students):
         students_distribution[i] += 1
     return students_distribution
@@ -28,7 +28,8 @@ def make_groups_lv(Data, promo_pair):
         slot_count = function_database.get_lv_slot_count(Data, promo_list)
         list_lv2 = function_database.find_list_LV2(Data, promo_list)
         for lv2 in list_lv2:
-            slot_nbr_for_lv, available_teacher = function_database.get_available_teacher(Data, slot_count, lv2)      
+            slot_nbr_for_lv, available_teacher = function_database.get_available_teacher(Data, slot_count, lv2)    
+            print(slot_nbr_for_lv)  
             #students_in_promo_pair = function_database.get_students_count(Data, promo_pair) 
             group = function_database.get_all_students_from_a_pair_and_lv2(Data, promo_list, lv2)
             #lv2_slot_count = function_database.get_lv2_slot_count(Data, lv2)
@@ -37,7 +38,7 @@ def make_groups_lv(Data, promo_pair):
             if "-débutant (jamais étudié)" in lv2 :
                 group = [pos[0] for pos in group]
                 name = "_D"   
-            students_distribution = get_students_per_class(len(group),slot_nbr_for_lv)
+            students_distribution = get_students_per_SCHOOL_YEAR(len(group),slot_nbr_for_lv)
             groups = make_groups(group, students_distribution)
             i = 1
             for group in groups:
