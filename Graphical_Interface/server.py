6aa3836e45402e9d56e579be1a6ad83416af3160
tgimond <theo.gimond@epfedu.fr>
2024-06-06 14:02:02 +0200

    Ajout de l'endpoint /professors fonctionnel

--- Graphical_Interface/server.py
+++ Graphical_Interface/server.py
@@ -109,8 +109,4 @@ def get_groups():
     return jsonify(groups)
 
 if __name__ == '__main__':
-    def open_browser():
-        webbrowser.open_new('http://127.0.0.1:5000')
-
-    threading.Timer(1, open_browser).start()
     app.run(debug=True)
