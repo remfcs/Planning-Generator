c0ca0ebd4f4c6fec6adcea7fa88fd1f85acb8297
marionddlr <marion.dumasdelaroque@epfedu.fr>
2024-06-07 00:06:53 +0200

    Mise en commun

--- Graphical_Interface/server.py
+++ Graphical_Interface/server.py
@@ -3,8 +3,85 @@ from flask import Flask, jsonify, request, send_from_directory
 import threading
 import webbrowser
 import os
+import subprocess
+import json
 
 app = Flask(__name__, static_url_path='', static_folder='.')
+
+# Définir le dossier de téléchargement
+UPLOAD_FOLDER = '././data/uploads'
+UPLOAD_FOLDER_LEVEL = '././data/uploads/input_level'
+UPLOAD_FOLDER_INFO = '././data/uploads/input_info'
+TEACHERS_JSON_PATH = os.path.join(UPLOAD_FOLDER, 'teachers.json')
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER_INFO'] = UPLOAD_FOLDER_INFO
+app.config['UPLOAD_FOLDER_LEVEL'] = UPLOAD_FOLDER_LEVEL
+
+# Créer le dossier de téléchargement s'il n'existe pas
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+if not os.path.exists(UPLOAD_FOLDER_LEVEL):
+    os.makedirs(UPLOAD_FOLDER_LEVEL)
+if not os.path.exists(UPLOAD_FOLDER_INFO):
+    os.makedirs(UPLOAD_FOLDER_INFO)
+
+@app.route('/start')
+def index():
+    return send_from_directory('.', 'start/start.html')
+
+@app.route('/start/submit', methods=['POST'])
+def submit():
+    # Récupérer les valeurs des champs de formulaire
+    estimate_number_student = request.form.get('estimate_number_student')
+    halfday_slot = request.form.get('halfday_slot')
+
+    # Récupérer les fichiers uploadés
+    students_files = request.files.getlist('students_files[]')
+    level_files = request.files.getlist('level_files[]')
+
+    # Enregistrer les fichiers uploadés
+    saved_files = {'students_files': [], 'level_files': []}
+
+    for file in students_files:
+        if file.filename:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER_INFO'], file.filename)
+            file.save(file_path)
+            saved_files['students_files'].append(file.filename)
+
+    for file in level_files:
+        if file.filename:
+            file_path = os.path.join(app.config['UPLOAD_FOLDER_LEVEL'], file.filename)
+            file.save(file_path)
+            saved_files['level_files'].append(file.filename)
+            
+    teachers_json = request.form.get('teachers', '[]')  # Default to an empty list as JSON
+    teachers = json.loads(teachers_json)  # Decode the JSON string
+    with open(TEACHERS_JSON_PATH, 'w') as json_file:
+        json.dump(teachers, json_file, indent=4)
+
+# Afficher les données pour vérification (peut être remplacé par une autre logique)
+    response_data = {
+    "estimation": f"Estimation du nombre de nouveaux étudiants: {estimate_number_student}",
+    "halfday_slots": f"Nombre de demi-journées: {halfday_slot}",
+    "students_files": ", ".join(saved_files['students_files']),
+    "level_files": ", ".join(saved_files['level_files']),
+    "teacher:": teachers
+    }    
+    return jsonify(response_data)
+
+@app.route('/create_planning', methods=['POST'])
+def create_planning_route():
+    try:
+        # Exécuter le script main.py
+        result = subprocess.run(['python', '././main.py'], capture_output=True, text=True)
+        
+        if result.returncode == 0:
+            return jsonify({"status": "success", "message": result.stdout.strip()})
+        else:
+            return jsonify({"status": "error", "message": result.stderr.strip()})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
 
 DATABASE_PATH = os.path.join(os.path.dirname(__file__), 'database.sqlite3')
 
