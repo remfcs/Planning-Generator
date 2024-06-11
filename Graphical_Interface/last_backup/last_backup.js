b25f647eb516b0b258edfa18c846d99df8ed7149
Justine <justine.sammut@epfedu.fr>
2024-06-11 15:45:13 +0200

    ajout last_backup

---
+++ Graphical_Interface/last_backup/last_backup.js
@@ -0,0 +1,25 @@
+document.getElementById('yes-btn').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+function confirmRecovery() {
+    fetch('/restore_backup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+        closePopup();
+    })
+    .catch(error => {
+        alert('An error occurred: ' + error.message);
+        closePopup();
+    });
+}
